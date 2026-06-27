6abdf2875270bf46b0bc51d0f6c181245c363cd2
OdooFillForm <intergrityfull@gmail.com>
2026-06-27 14:44:20 +0700

    fix: 替换为正确的 anon key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://vlettaxewktmnubsmkbv.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_gMJLo2noMvTiC9wl2rTc5Q_Jvf2hQQV'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsZXR0YXhld2t0bW51YnNta2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1ODA4NzgsImV4cCI6MjA5NzE1Njg3OH0.GVolWyDGImO23XLVeIHuM9A-lNNvUZ-NCF43Eo6CxvM'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
